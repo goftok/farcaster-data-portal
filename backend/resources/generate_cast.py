4e566a4da0294bb20053fa6230992c979ae69d59
Ilya Dashkevich aws <ubuntu@ip-172-31-30-57.eu-west-1.compute.internal>
2024-09-08 08:32:29 +0000

    fixing heroku

--- backend/resources/generate_cast.py
+++ backend/resources/generate_cast.py
@@ -3,12 +3,12 @@ from flask_restful import Resource
 from flasgger import swag_from
 from werkzeug.exceptions import BadRequest
 
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-from transformers_models.generate_cast.predict import predict_cast
-from backend.env_links import MODEL_PATH
+# from transformers import T5ForConditionalGeneration, T5Tokenizer
+# from transformers_models.generate_cast.predict import predict_cast
+# from backend.env_links import MODEL_PATH
 
-model = T5ForConditionalGeneration.from_pretrained(MODEL_PATH)
-tokenizer = T5Tokenizer.from_pretrained(MODEL_PATH, legacy=False)
+# model = T5ForConditionalGeneration.from_pretrained(MODEL_PATH)
+# tokenizer = T5Tokenizer.from_pretrained(MODEL_PATH, legacy=False)
 max_length = 250
 
 
@@ -20,10 +20,10 @@ class GenerateCast(Resource):
             if "keywords" not in data or not isinstance(data["keywords"], str):
                 raise BadRequest("Invalid 'keywords' key or value in request body.")
 
-            # return {"error": "Model is not hosted nad and there is no endpoint  for the model."}, 500
+            return {"error": "Model is not hosted nad and there is no endpoint  for the model."}, 500
 
-            predicted_cast = predict_cast(data["keywords"], model, tokenizer, max_length)
-            return jsonify({"predicted_cast": predicted_cast})
+            # predicted_cast = predict_cast(data["keywords"], model, tokenizer, max_length)
+            # return jsonify({"predicted_cast": predicted_cast})
 
         except BadRequest as e:
             return {"error": str(e)}, 400
